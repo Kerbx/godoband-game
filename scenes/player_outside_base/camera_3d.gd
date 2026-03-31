7dff09c89d6e4ec9993e7016d8f546f31b8b0148
maxim <kuryatkov.maxim@gmail.com>
2026-03-31 11:02:13 +0300

    camera follows ship in interact mode

--- scenes/player_outside_base/camera_3d.gd
+++ scenes/player_outside_base/camera_3d.gd
@@ -9,16 +9,17 @@ var offset: float = 0
 var pinned: bool = false
 
 func _process(delta: float) -> void:
+	global_position = follow_object.global_position + follow_object.basis.z * offset + Vector3(0,height,0)
+	if pinned:
+		look_at(follow_object.position)
 
-	global_position = Vector3(follow_object.global_position.x, follow_object.global_position.y + height, follow_object.global_position.z + offset)
-	look_at(follow_object.position)
 
 
 func _on_player_outside_base_interact_mode_enter(asteroid: Asteroid) -> void:
 	print('change camera position')
 	camera_tween = get_tree().create_tween()
 	
-	camera_tween.parallel().tween_property(self, "height", height/2, 0.5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	camera_tween.parallel().tween_property(self, "offset", -5.0, 0.5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	camera_tween.parallel().tween_property(self, "height", 7.0, 0.5).set_trans(Tween.TRANS_CUBIC)
+	camera_tween.parallel().tween_property(self, "offset", 5.0, 0.5).set_trans(Tween.TRANS_CUBIC)
 	
 	pinned = true
